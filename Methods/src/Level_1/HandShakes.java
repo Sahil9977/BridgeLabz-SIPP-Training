120fbfe36c45ff7eb6aa648fb14e1be19b5495c8
sahil9977 <factstar1290@gmail.com>
2025-06-21 18:37:42 +0530

    [Sahil] Methods

---
+++ Methods/src/Level_1/HandShakes.java
@@ -0,0 +1,15 @@
+package Level_1;
+import java.util.*;
+public class HandShakes {
+
+	public static int calculateHandshakes(int numberOfStudents) {
+        return (numberOfStudents * (numberOfStudents - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStudents = scanner.nextInt();
+        int handshakes = calculateHandshakes(numberOfStudents);
+        System.out.println("Handshakes: " + handshakes);
+    }
+}
